9d643bcbafc6c1d9fe916e6ffce73f6e8c178ca9
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2020-04-02 01:25:29 +0900

    initial commit

---
+++ src/test/kotlin/com/b1a9idps/clotheshouse/ClothesHouseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.b1a9idps.clotheshouse
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClothesHouseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
