ff265c802b597286d78f1f3652cb76cbf60cca46
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2020-04-13 16:19:18 +0900

    H2利用

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 //	implementation("org.flywaydb:flyway-core")
+	implementation("com.h2database:h2:1.4.199")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 //	runtimeOnly("org.postgresql:postgresql")
