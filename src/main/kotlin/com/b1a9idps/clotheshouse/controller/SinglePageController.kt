74617f993570fdfb5598a1f89008f3f6209290f4
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2020-04-13 10:00:44 +0900

    不要な修飾子削除

--- src/main/kotlin/com/b1a9idps/clotheshouse/controller/SinglePageController.kt
+++ src/main/kotlin/com/b1a9idps/clotheshouse/controller/SinglePageController.kt
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-public class SinglePageController {
+class SinglePageController {
     @GetMapping("/**/{path:[^.]*}")
     fun index(): String {
         return "forward:/index.html";
