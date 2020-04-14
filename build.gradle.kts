19b263990e59b2f35ac413e38c8cb30ab15542b0
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2020-04-15 00:23:46 +0900

    Jib導入

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("org.springframework.boot") version "2.2.6.RELEASE"
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
 	id("com.github.node-gradle.node") version "2.2.3"
+	id("com.google.cloud.tools.jib") version "2.1.0"
 	kotlin("jvm") version "1.3.71"
 	kotlin("plugin.spring") version "1.3.71"
 }
