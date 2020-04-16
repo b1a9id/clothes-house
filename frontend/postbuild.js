5e83bbd38e4bbeac24c484a604d3f4d40ee232e2
Ryosuke Uchitate <hello.ryosuke.u@gmail.com>
2020-04-16 23:53:47 +0900

    bug fix (#13) (#14)

--- frontend/postbuild.js
+++ frontend/postbuild.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require("fs-extra");
 
 const env = process.env.GITHUB_WORKSPACE;
-const baseDir = env.length !== 0 ? env : __dirname;
+const baseDir = env !== null ? env : __dirname;
 const BUILD_DIR = path.join(baseDir, './build');
 const PUBLIC_DIR = path.join(baseDir, '../src/main/resources/public');
 
