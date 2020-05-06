9115b636cc47d1e4146498fc91e5ca91889c2a20
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2020-05-07 00:43:03 +0900

    big fix

--- frontend/postbuild.js
+++ frontend/postbuild.js
@@ -2,8 +2,8 @@ const path = require('path');
 const fs = require("fs-extra");
 
 console.log('########## __dirname =' + __dirname);
-const BUILD_DIR = path.join(baseDir, './build');
-const PUBLIC_DIR = path.join(baseDir, '../src/main/resources/public');
+const BUILD_DIR = path.join(__dirname, './build');
+const PUBLIC_DIR = path.join(__dirname, '../src/main/resources/public');
 console.log('########## BUILD_DIR =' + BUILD_DIR);
 console.log('########## PUBLIC_DIR =' + PUBLIC_DIR);
 
