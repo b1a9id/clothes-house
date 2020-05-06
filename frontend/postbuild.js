e33a2e4b00b0d290f83d4626094f538004101cc6
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2020-05-07 00:49:01 +0900

    デバッグ用コード削除

--- frontend/postbuild.js
+++ frontend/postbuild.js
@@ -1,11 +1,8 @@
 const path = require('path');
 const fs = require("fs-extra");
 
-console.log('########## __dirname =' + __dirname);
 const BUILD_DIR = path.join(__dirname, './build');
 const PUBLIC_DIR = path.join(__dirname, '../src/main/resources/public');
-console.log('########## BUILD_DIR =' + BUILD_DIR);
-console.log('########## PUBLIC_DIR =' + PUBLIC_DIR);
 
 fs.emptyDirSync(PUBLIC_DIR);
 fs.copySync(BUILD_DIR, PUBLIC_DIR);
