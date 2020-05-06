4e73164787bd9d46fe04c4cc9ee763af8ee5deff
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2020-05-07 00:37:46 +0900

    パス変更

--- frontend/postbuild.js
+++ frontend/postbuild.js
@@ -1,12 +1,9 @@
 const path = require('path');
 const fs = require("fs-extra");
 
-const prodEnv = process.env.GITHUB_WORKSPACE;
-console.log('########## env =' + prodEnv);
-const baseDir = prodEnv !== undefined ? prodEnv : __dirname;
 console.log('########## __dirname =' + __dirname);
 const BUILD_DIR = path.join(baseDir, './build');
-const PUBLIC_DIR = path.join(baseDir, prodEnv !== undefined ? './src/main/resources/public' : '../src/main/resources/public');
+const PUBLIC_DIR = path.join(baseDir, '../src/main/resources/public');
 console.log('########## BUILD_DIR =' + BUILD_DIR);
 console.log('########## PUBLIC_DIR =' + PUBLIC_DIR);
 
