a747cc6d9a8410f8c790ac1822f4894a88dcefd5
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2020-05-06 23:58:06 +0900

    デバッグ用出力

--- frontend/postbuild.js
+++ frontend/postbuild.js
@@ -2,9 +2,13 @@ const path = require('path');
 const fs = require("fs-extra");
 
 const env = process.env.GITHUB_WORKSPACE;
+console.log('########## env =' + env);
 const baseDir = env !== undefined ? env : __dirname;
+console.log('########## __dirname =' + __dirname);
 const BUILD_DIR = path.join(baseDir, './build');
 const PUBLIC_DIR = path.join(baseDir, '../src/main/resources/public');
+console.log('########## BUILD_DIR =' + BUILD_DIR);
+console.log('########## PUBLIC_DIR =' + PUBLIC_DIR);
 
 fs.emptyDirSync(PUBLIC_DIR);
 fs.copySync(BUILD_DIR, PUBLIC_DIR);
