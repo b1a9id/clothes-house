20418d64cf58382218de7ff1e7e93013c201eee0
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2020-05-17 22:52:16 +0900

    fix typo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ResponsiveDrawer from "./conponents/ResponsiveDrawer";
+import ResponsiveDrawer from "./components/ResponsiveDrawer";
 
 function App() {
   return (
