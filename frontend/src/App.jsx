f45d734c0080deacaec8b7b84e2069cf91c78812
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2020-05-17 23:49:16 +0900

    AppBar 追加

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { Route, Switch } from "react-router";
 import Closet from "./components/Closet";
 import Setting from "./components/Setting";
 import Nav from "./containers/Nav";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <Switch>
-        <Route exact path="/" component={Closet} />
-        <Route path="/settings" component={Setting} />
-      </Switch>
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CssBaseline />
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography variant="h6" noWrap>
+              Clothe House
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Closet} />
+          <Route path="/settings" component={Setting} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
