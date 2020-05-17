1e7bbcab83402bd8f14e929cdd7c4fc672774ba3
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2020-05-17 23:13:42 +0900

    ルーティング実装

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
-import ResponsiveDrawer from "./components/ResponsiveDrawer";
+import { Route, Switch } from "react-router";
+import { Link } from "react-router-dom";
+import Closet from "./components/Closet";
+import Setting from "./components/Setting";
 
 function App() {
   return (
-    <ResponsiveDrawer />
+    <div className="App">
+      <ul>
+        <li><Link to="/">Closet</Link></li>
+        <li><Link to="/settings">Setting</Link></li>
+      </ul>
+      <Switch>
+        <Route exact path="/" component={Closet} />
+        <Route path="/settings" component={Setting} />
+      </Switch>
+    </div>
   )
 }
 
