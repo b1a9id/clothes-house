1a457b1b717cfbd13f11e28385041dee1c493d6a
Ryosuke <ry0suke.uch1tate@gmail.com>
2020-07-09 20:26:07 +0900

    Refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,11 +44,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.flywaydb:flyway-core")
-	implementation("org.postgresql:postgresql")
+	runtimeOnly("org.postgresql:postgresql")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-//	runtimeOnly("org.postgresql:postgresql")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
@@ -101,6 +101,19 @@ tasks.jacocoTestReport {
 		xml.destination = file("${buildDir}/reports/jacoco/report.xml")
 		csv.isEnabled = false
 		html.isEnabled = false
+	}
+	afterEvaluate {
+		classDirectories.setFrom(
+				files(classDirectories.files.map {
+					fileTree(it).apply {
+						exclude(
+								"**/annotation/**",
+								"**/exception/**",
+								"**/Application.kt"
+						)
+					}
+				})
+		)
 	}
 }
 
